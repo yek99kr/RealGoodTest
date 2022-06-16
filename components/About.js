3d204f27ebe8a1f49b93cfb5f94eeac92c709b2e
Rachel Kim <yek99kr@gmal.com>
2022-06-16 16:36:29 -0400

    a

--- components/About.js
+++ components/About.js
@@ -107,7 +107,7 @@ const About = (props) => {
   return (
     <>
       {!props.isSmallerHeight ? (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[63%] leading-[2] md:leading-[2.2] lg:leading-[1.9] w-[87vw] sm:w-[85vw] md:w-[80vw] lg:w-[70vw] xl:w-[65vw] 2xl:w-[58vw] text-[3vw] 4xs:text-[2.9vw] sm:text-[16px] md:text-[17px] lg:text-[18.5px] xl:text-[19.5px] 2xl:text-[23px] text-center">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[63%] leading-[2] md:leading-[2.2] lg:leading-[1.9] w-[87vw] sm:w-[85vw] md:w-[80vw] lg:w-[68vw] xl:w-[63vw] 2xl:w-[60vw] text-[3vw] 4xs:text-[2.9vw] sm:text-[16px] md:text-[17px] lg:text-[18.5px] xl:text-[19.5px] 2xl:text-[23px] text-center">
           <p className="font-[400]">
             We&apos;re{" "}
             <img
@@ -145,15 +145,15 @@ const About = (props) => {
                 rel="noreferrer"
               >
                 <span className="insta cursor-pointer inline-block noselect thumbcursor">
-                  <span className="wiggleMobile1 text-pink-500">i</span>
-                  <span className="wiggleMobile2 text-sky-400">n</span>
-                  <span className="wiggleMobile3 text-yellow-500">s</span>
-                  <span className="wiggleMobile4 text-purple-500">t</span>
-                  <span className="wiggleMobile5 text-pink-500">a</span>
-                  <span className="wiggleMobile6 text-sky-400">g</span>
-                  <span className="wiggleMobile7 text-pink-500">r</span>
-                  <span className="wiggleMobile8 text-yellow-500">a</span>
-                  <span className="wiggleMobile9 text-green-500">m</span>
+                  <span className="wiggle1 text-pink-500">i</span>
+                  <span className="wiggle2 text-sky-400">n</span>
+                  <span className="wiggle3 text-yellow-500">s</span>
+                  <span className="wiggle4 text-purple-500">t</span>
+                  <span className="wiggle5 text-pink-500">a</span>
+                  <span className="wiggle6 text-sky-400">g</span>
+                  <span className="wiggle7 text-pink-500">r</span>
+                  <span className="wiggle8 text-yellow-500">a</span>
+                  <span className="wiggle9 text-green-500">m</span>
                 </span>
               </a>{" "}
               with some work and a{" "}
@@ -163,10 +163,10 @@ const About = (props) => {
                 rel="noreferrer"
               >
                 <span className="deck inline-block noselect thumbcursor">
-                  <span className="wiggleMobile1 text-purple-500">d</span>
-                  <span className="wiggleMobile2 text-yellow-500">e</span>
-                  <span className="wiggleMobile3 text-sky-400">c</span>
-                  <span className="wiggleMobile4 text-pink-500">k</span>
+                  <span className="wiggle1 text-purple-500">d</span>
+                  <span className="wiggle2 text-yellow-500">e</span>
+                  <span className="wiggle3 text-sky-400">c</span>
+                  <span className="wiggle4 text-pink-500">k</span>
                 </span>{" "}
               </a>
               with some more work.
